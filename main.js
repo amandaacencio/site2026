4020de31199b7fb8ea61c83efe00ac5d01b107af
amandaacencio <amanda.acencio@escola.pr.gov.br>
2026-04-06 11:40:18 -0300

    iiyiuyuy

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 const botoes = document.querySelectorAll ("botao");
 console.timeLog(botoes)
+
+for (let i=0; i <botoes.length; i++){
+    botoes[i] .onlick = function(){
+
+      for(let j=0; j<botoes.length; j++){
+        botoes[j].classList.remove("ativo");
+      }
+
+
+        botoes[i].classList.add("ativo");
+    }
+}
